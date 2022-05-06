ac703f8e0725adf8e38bb5526172f2644be72b01
SurajBhange <suraj.bhange.007@gmail.com>
2022-05-06 13:03:30 +0530

    first commit

---
+++ src/app/MyComponent/about/about.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  myimage:string = "assets/image/img9.jpg";
+
+}
